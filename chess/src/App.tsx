f623070ae870f1db581c247c42731beb0a0d328d
Saif <droconos99@gmail.com>
2025-07-25 23:53:13 +0600

    Fix: Setup

---
+++ chess/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+   return (
+      <div>
+         <div>
+            {/* Turn div */}
+            Turn: <div></div>
+         </div>
+
+         <div>{/* Board */}</div>
+
+         <div>{/* Took stuffs from p1 and p2 */}</div>
+      </div>
+   );
+}
+
+export default App;
