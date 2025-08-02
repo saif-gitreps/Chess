23d1cab1df7be485b24fcf60d44cd5bb105e71a2
Saif <droconos99@gmail.com>
2025-08-02 17:18:57 +0600

    Fix: Deployment

--- chess/src/utils/socket.ts
+++ chess/src/utils/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
-console.log(API_BASE_URL);
-const socket = io(API_BASE_URL); // Replace with your deployed backend
+
+const socket = io(API_BASE_URL);
 export default socket;
